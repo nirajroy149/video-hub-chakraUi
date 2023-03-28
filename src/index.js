5b6d9fc7a78bd4587ffc76e52f7709ecead64c5a
Niraj Kumar Roy <nirajroy149@gmail.com>
2023-03-28 12:29:14 +0530

    git error pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { ColorModeScript, ChakraProvider, theme } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import ColorModeSwitcher from './ColorModeSwitcher'
+import ColorModeSwitcher from './ColorModeSwitcher';
 
 
 const container = document.getElementById('root');
